14e16943f0b54a5837e7534d80fbd880d2e83d31
LeahStevens <leiastevens@hotmail.com>
2013-10-17 16:41:58 -0500

    changePrice function

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,68 +1,68 @@
-// 'use strict';
+'use strict';
 
-// // Firebase Schema
-// var Δdb;
 
-// // Local Schema (defined in keys.js)
+// Local Schema (defined in keys.js)
 
-// $(document).ready(initialize);
+$(document).ready(initialize);
 
-// function initialize(fn, flag){
-//   if(!canRun(flag)) {return;}
+function initialize(fn, flag){
+  if(!canRun(flag)) {return;}
+  $('#createSeats').click(clickCreateSeats);
+  $('#vip').click(changeVipPrice);
+  $('#gen-adm').click(changeGeneralAdmissionPrice);
+}
 
-//   $(document).foundation();
-//   Δdb = new Firebase(db.keys.firebase);
-//   initMap(36, -86, 5);
-// }
+// ------------------------------Click Handlers---------------------------------- //
+function clickCreateSeats(){
 
-// // -------------------------------------------------------------------- //
-// // -------------------------------------------------------------------- //
-// // -------------------------------------------------------------------- //
+}
 
-// function initMap(lat, lng, zoom){
-//   var mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP};
-//   db.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-// }
+function changeVipPrice(){
+  debugger;
+  $('#seatPrice').val('$110.00');
+}
+function changeGeneralAdmissionPrice(){
+  $('#seatPrice').val('$90.00');
+}
 
-// // -------------------------------------------------------------------- //
-// // -------------------------------------------------------------------- //
-// // -------------------------------------------------------------------- //
 
-// function getValue(selector, fn){
-//   var value = $(selector).val();
-//   value = value.trim();
-//   $(selector).val('');
+// -------------------------------------------------------------------- //
 
-//   if(fn){
-//     value = fn(value);
-//   }
+function getValue(selector, fn){
+  var value = $(selector).val();
+  value = value.trim();
+  $(selector).val('');
 
-//   return value;
-// }
+  if(fn){
+    value = fn(value);
+  }
 
-// function parseUpperCase(string){
-//   return string.toUpperCase();
-// }
+  return value;
+}
 
-// function parseLowerCase(string){
-//   return string.toLowerCase();
-// }
+function parseUpperCase(string){
+  return string.toUpperCase();
+}
 
-// function formatCurrency(number){
-//   return '$' + number.toFixed(2);
-// }
+function parseLowerCase(string){
+  return string.toLowerCase();
+}
 
-// // -------------------------------------------------------------------- //
-// // -------------------------------------------------------------------- //
-// // -------------------------------------------------------------------- //
+function formatCurrency(number){
+  return '$' + number.toFixed(2);
+}
 
-// function canRun(flag){
-//   var isQunit = $('#qunit').length > 0;
-//   var isFlag = flag !== undefined;
-//   var value = isQunit && isFlag || !isQunit;
-//   return value;
-// }
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
 
-// // -------------------------------------------------------------------- //
-// // -------------------------------------------------------------------- //
-// // -------------------------------------------------------------------- //
+function canRun(flag){
+  var isQunit = $('#qunit').length > 0;
+  var isFlag = flag !== undefined;
+  var value = isQunit && isFlag || !isQunit;
+  return value;
+}
+
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
--- public/js/tests/master.js
+++ public/js/tests/master.js
@@ -1,14 +1,43 @@
-// 'use strict';
+'use strict';
 
-// module('Integration Testing', {setup: setupTest, teardown: teardownTest});
+module('Integration Testing', {setup: setupTest, teardown: teardownTest});
 
-// function setupTest(){
-//   initialize(null, true);
-// }
+function setupTest(){
+  initialize(null, true);
+}
 
-// function teardownTest(){
-// }
+function teardownTest(){
+}
 
-// test('<name-of-feature>', function(){
-//   expect(1);
-// });
+test('Select Correct Section', function(){
+  expect(2);
+
+
+  $('#vip').val('VIP Section');
+  deepEqual($('#vip').val(),'VIP Section','VIP Section should be the result');
+  $('#CreateSeats').trigger('click');
+
+  $('#gen-adm').val('General Admission');
+  deepEqual($('#gen-adm').val(),'General Admission','General Admission should be the result');
+  $('#CreateSeats').trigger('click');
+});
+
+test('Correct Number of Seats', function(){
+  expect(1);
+  $('#seatNumber').val('35');
+  deepEqual($('#seatNumber').val(),'35','The number of seats should be 35');
+  $('#CreateSeats').trigger('click');
+});
+
+test('Correct Price of Section Seats', function(){
+  expect(2);
+
+  $('#seatPrice').val('$110.00');
+  deepEqual($('#seatPrice').val(),'$110.00','The price of VIP Seats should be $110.00 ');
+  $('#CreateSeats').trigger('click');
+
+  $('#seatPrice').val('$90.00');
+  deepEqual($('#seatPrice').val(),'$90.00','The price of General Admission Seats should be $90.00 ');
+  $('#CreateSeats').trigger('click');
+
+});
